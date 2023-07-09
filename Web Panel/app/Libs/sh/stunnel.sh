453dd89c0b5e6947f23d92bf26d9b96c6f440548
Alireza <Alirezadb1994@gmail.com>
2023-07-09 07:48:05 +0330

    Fix Version 3.6

--- Web Panel/app/Libs/sh/stunnel.sh
+++ Web Panel/app/Libs/sh/stunnel.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-accept=1
-connect=2
-if echo "$accept" | grep -qE '^[a-zA-Z0-9]+$'; then
-   accept=$1
+if [[ "$1" =~ [^a-zA-Z0-9] ]]; then
+accept=444
+else
+accept=$1
 fi
-if echo "$connect" | grep -qE '^[a-zA-Z0-9]+$'; then
-   connect=$2
+
+if [[ "$2" =~ [^a-zA-Z0-9] ]]; then
+accept=22
+else
+accept=$2
 fi
 echo "cert = /etc/stunnel/stunnel.pem
  [openssh]
