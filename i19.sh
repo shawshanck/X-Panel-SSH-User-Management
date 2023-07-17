75fe41248234e00d08d704a36597868012fcaeb0
Alireza <127631174+Alirezad07@users.noreply.github.com>
2023-07-17 18:41:00 +0330

    Update i19.sh

--- i19.sh
+++ i19.sh
@@ -380,7 +380,29 @@ wait
 systemctl restart stunnel4 &
 wait
 curl -o /root/xpanel.sh https://raw.githubusercontent.com/Alirezad07/X-Panel-SSH-User-Management/main/cli.sh
+#Alireza & Roozemain
+sudo apt-get install curl unzip perl
+sudo apt-get install xtables-addons-common -y
+sudo apt-get install libtext-csv-xs-perl libmoosex-types-netaddr-ip-perl -y
+wait
+sudo mkdir /usr/share/xt_geoip
+sudo mkdir /usr/libexec/xtables-addons/
+sudo mkdir /usr/libexec/xtables-addons/xt_geoip_build
+chmod +x /usr/libexec/xtables-addons/xt_geoip_build
+wait
+MON=$(date +"%m")
+YR=$(date +"%Y")
 
+wget https://download.db-ip.com/free/dbip-country-lite-${YR}-${MON}.csv.gz
+cp dbip-country-lite-${YR}-${MON}.csv.gz /usr/share/xt_geoip/
+rm dbip-country-lite-${YR}-${MON}.csv.gz
+gunzip /usr/share/xt_geoip/dbip-country-lite-${YR}-${MON}.csv.gz
+/usr/libexec/xtables-addons/xt_geoip_build -D /usr/share/xt_geoip/ -i /usr/share/xt_geoip/dbip-country-lite-${YR}-${MON}.csv
+modprobe xt_geoip
+lsmod | grep ^xt_geoip
+wait
+echo -e "\n Download Success GEOIP Library  \n"
+sleep 3
 clear
 
 echo -e "************ XPanel ************ \n"
