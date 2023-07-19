b8766b44c5ff1f02c372fac900875a94b53448c5
Alireza <Alirezadb1994@gmail.com>
2023-07-19 17:00:12 +0330

    Fix Version 3.7.2 Laravel Package

--- Web Panel/app/resources/views/layouts/footer.blade.php
+++ Web Panel/app/resources/views/layouts/footer.blade.php
@@ -24,7 +24,7 @@
 <script src="/assets/js/plugins/simplebar.min.js"></script>
 <script src="/assets/js/plugins/bootstrap.min.js"></script>
 <script src="/assets/js/fonts/custom-font.js"></script>
-<script src="/assets/js/config.js?v=3.7.1"></script>
+<script src="/assets/js/config.js?v=3.7.2"></script>
 <script src="/assets/js/pcoded.js"></script>
 <script src="/assets/js/plugins/feather.min.js"></script>
 <!-- [Page Specific JS] start -->
